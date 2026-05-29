e98b7d17c387644d8d68513ae930d3dbfb981a99
lavyadav182 <lavkumaryadav824@gmail.com>
2026-05-29 21:13:48 +0530

    add

--- dash/src/environment.js
+++ dash/src/environment.js
@@ -1,10 +1,10 @@
 
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://notess-ucwp.onrender.com":
-    "http://localhost:3000"
+    // "https://notess-ucwp.onrender.com":
+    "https://store-gpjk.onrender.com":
+    "http://localhost:5000"
 
-  
 export default server;
 
 
