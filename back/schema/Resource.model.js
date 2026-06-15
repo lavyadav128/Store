916aea3742b3868acff2577bb2b2613b4a310b23
lavyadav182 <lavkumaryadav824@gmail.com>
2026-06-15 10:33:20 +0530

    add

--- back/schema/Resource.model.js
+++ back/schema/Resource.model.js
@@ -14,14 +14,12 @@ const resourceSchema = new mongoose.Schema(
       required: true,
       enum: [
         "pyq",
-        "mindmap",
-        "shortnotes",
-        "fullstack",
-        "dsa_files",
         "completenotes",
         "videos",
         "motivation",
         "projects",
+        "reels",
+        "shorts",
       ],
     },
 
