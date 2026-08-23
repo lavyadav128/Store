e986025ea5c48300c3f1e2da7d4539125e099fa4
lavyadav182 <lavkumaryadav824@gmail.com>
2026-08-23 16:01:15 +0530

    add

--- back/schema/UserChapterNote.model.js
+++ back/schema/UserChapterNote.model.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
   batchSlug: { type: String, required: true },
   subjectSlug: { type: String, required: true },
   chapterSlug: { type: String, required: true },
-  noteText: { type: String, required: true, maxlength: 20000 },
+  noteText: { type: String, required: true, maxlength: 500000 },
 }, { timestamps: true });
 
 schema.index({ userId: 1, batchSlug: 1, subjectSlug: 1, chapterSlug: 1 }, { unique: true });
